a06ee8fa26e5301a4b307a5e2632d29da3513464
Arthur Ashman <arthurashman@gmail.com>
2020-06-15 21:16:25 +0100

    return all users from external api

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const request = require('request-promise');
+
+let users = null
+
+var options = {
+  uri: 'https://bpdts-test-app.herokuapp.com/users',
+  json: true
+}
+
+request(options)
+  .then(data => {
+     users = data
+  })
 
 const app = express();
 
-app.get('/', (req, res) => res.status(200).send('Hello world!'));
+app.get('/', (req, res) => res.status(200).send(users));
 
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
-module.exports = app;
+module.exports = app
--- test/app.test.js
+++ test/app.test.js
@@ -2,25 +2,13 @@ const request = require('supertest')
 const app = require('../app')
 
 describe('app', function(){
-
-  let agent = null
-  let server = null
-
-  beforeEach((done) => {
-    server = app.listen(3000, (err) => {
-      if (err) return done(err);
-       agent = request.agent(server);
-       done();
-    });
+  it("returns all users and status code 200", function(done) {
+    request(app)
+      .get("/")
+      .expect(200)
+      .end(function(err, res) {
+        res.body.length == 1000
+        done();
+      });
   });
-
-  afterEach((done) => {
-    server.close(done);
-  });
-
-  it('connects to server /', function(){
-    return agent.get('/')
-      .expect(200, "Hello world!")
-  })
-  
 })
