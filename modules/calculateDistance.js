f58a4ca84114d2715f07ddfa912c182137708550
Arthur Ashman <arthurashman@gmail.com>
2020-06-15 21:21:44 +0100

    calculateDistance returns correct value when coordinates differ

--- modules/calculateDistance.js
+++ modules/calculateDistance.js
@@ -1,4 +1,8 @@
 module.exports = function(lat, lon, lat_center, lon_center){
-  return 0;
+  return (3956.5 * Math.acos(Math.cos(radians(lat)) * Math.cos(radians(lat_center)) * Math.cos(radians(lon_center) - radians(lon)) + Math.sin(radians(lat)) * Math.sin(radians(lat_center))));
 }
 
+const radians = degrees => {
+  var TAU = 2 * Math.PI;
+  return degrees * TAU / 360;
+}
--- test/calculateDistance.test.js
+++ test/calculateDistance.test.js
@@ -6,4 +6,16 @@ describe('#calculateDistance()', function() {
     it('should return 0 when centre coordinates equal user coordinates', function() {
       expect(calculateDistance(lat = 0, lon = 0, lat_center = 0, lon_center = 0)).to.equal(0)
     })
+    
+    it('should return correct distance for differing latitudes', function() {
+      expect(calculateDistance(lat = -1, lon = 3, lat_center =-2, lon_center = 3)).to.equal(69.05395185514875)
+    })
+
+    it('should return correct distance for differing longitudes', function() {
+      expect(calculateDistance(lat = -1, lon = 1, lat_center =-1, lon_center = 3)).to.equal(138.0868670720206)
+    })
+
+    it('should return correct distance for differing coordinates', function() {
+      expect(calculateDistance(lat = -1, lon = 1, lat_center =-2, lon_center = 3)).to.equal(154.3654273546793)
+    })
   })
